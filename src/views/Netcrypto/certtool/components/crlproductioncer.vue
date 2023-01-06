fc9d4d743e93d7c3f100422ef8b8c0a342df959a
huangyuanyin <3443718488@qq.com>
2023-01-06 15:47:47 +0800

    crl文件上传tab

--- src/views/Netcrypto/certtool/components/crlproductioncer.vue
+++ src/views/Netcrypto/certtool/components/crlproductioncer.vue
@@ -1,4 +1,44 @@
 <template>
+  <div class="rootKey">
+    <h5>根密钥</h5>
+    <el-card shadow="never">
+      <el-form
+        :model="rootKeyDNForm"
+        :rules="rootKeyDNFormRules"
+        ref="rootKeyDNFormRef"
+        class="rootKeyDNForm"
+        label-width="130px"
+        size="default"
+      >
+        <el-form-item label="根证书DN" prop="rootKeyDN">
+          <el-input
+            v-model="rootKeyDNForm.rootKeyDN"
+            placeholder="（假数据）C=cn,O=INFOSEC Technologies SHA256ID,CN=appSHA256ID"
+            style="width: 30vw"
+            disabled
+          />
+          <el-button type="primary" style="margin-left: 20px">下载根证</el-button>
+          <el-upload
+            ref="upload"
+            class="uploadDN"
+            action="myfile"
+            :on-change="handleRootKeyChange"
+            :limit="1"
+            :on-exceed="handleRootKeyExceed"
+            accept=".pem,.pfx"
+            :file-list="fileRootKeyList"
+            :auto-upload="false"
+          >
+            <el-button type="primary" style="margin-left: 20px">自定义根证</el-button>
+          </el-upload>
+          <el-button size="default" style="margin-left: 20px">选回默认证书</el-button>
+        </el-form-item>
+        <el-form-item label="说明：" class="illustrate">
+          <span class="illustrate">可根据需要自定义根证书，否则将使用默认根证签名，请知晓！</span>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
   <div class="numberCer">
     <h5>生成CRL文件数量</h5>
     <el-card shadow="never">
@@ -15,7 +55,7 @@
       <el-form :model="informationCRLForm" :rules="informationCRLFormRules" ref="informationCRLFormRef" label-width="130px" size="large">
         <el-row :gutter="24">
           <el-col :span="8">
-            <el-form-item label="证书文件">
+            <!-- <el-form-item label="证书文件">
               <el-upload
                 ref="upload"
                 class="upload-demo"
@@ -32,12 +72,18 @@
                 <i class="el-icon-upload"></i>
                 <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
               </el-upload>
-            </el-form-item>
+            </el-form-item> -->
             <el-form-item label="证书密钥口令" prop="pfxpass">
               <el-input v-model="informationCRLForm.pfxpass" placeholder="请输入证书密钥口令" style="width: 250px" />
             </el-form-item>
             <el-form-item label="证书个数" prop="cersercount">
               <el-input v-model="informationCRLForm.cersercount" placeholder="请输入证书个数" style="width: 250px" />
+            </el-form-item>
+            <el-form-item label="证书起始序列号" prop="certserial">
+              <el-input v-model="informationCRLForm.certserial" placeholder="请输入证书起始序列号" style="width: 250px" />
+            </el-form-item>
+            <el-form-item label="CRL文件序号" prop="crlnumberfile">
+              <el-input v-model="informationCRLForm.crlnumberfile" placeholder="请输入CRL文件序号" style="width: 250px" />
             </el-form-item>
           </el-col>
           <el-col :span="8">
@@ -49,12 +95,6 @@
                 format="YYYY/MM/DD"
                 value-format="YYYY-MM-DD"
               />
-            </el-form-item>
-            <el-form-item label="证书起始序列号" prop="certserial">
-              <el-input v-model="informationCRLForm.certserial" placeholder="" style="width: 250px" />
-            </el-form-item>
-            <el-form-item label="CRL文件序号" prop="crlnumberfile">
-              <el-input v-model="informationCRLForm.crlnumberfile" placeholder="" style="width: 250px" />
             </el-form-item>
             <el-form-item label="签名算法" prop="digetType">
               <el-select v-model="informationCRLForm.digetType" placeholder="请选择签名算法" style="width: 250px">
@@ -174,6 +214,14 @@ const files = ref([])
 const fileList = ref([]) // 上传文件
 const addExtstrlist = ref('0')
 const resultUrl = ref('')
+const fileRootKeyList = ref([])
+const rootKeyDNForm = reactive({
+  rootKeyDN: ''
+})
+const rootKeyDNFormRef = ref<FormInstance>()
+const rootKeyDNFormRules = reactive<FormRules>({
+  rootKeyDN: [{ required: true, message: '请输入根证书DN', trigger: 'blur' }]
+})
 const numberCRLForm = reactive({
   filenum: ''
 })
@@ -209,6 +257,16 @@ const handleExceed = (files, fileList) => {
 
 const handleChange = (file, fileList) => {
   files.value.push(file.raw)
+}
+
+const handleRootKeyChange = (file, fileList) => {
+  fileRootKeyList.value = []
+  fileRootKeyList.value.push(file.raw)
+}
+
+const handleRootKeyExceed = (files, fileList) => {
+  // ElMessage.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
+  ElMessage.warning(`一次只能上传1 个文件`)
 }
 
 const addExtstrlistForm = () => {
@@ -281,6 +339,18 @@ const generateCer2 = () => {
 </script>
 
 <style lang="scss" scoped>
+.rootKey {
+  .illustrate {
+    font-weight: 600;
+    :deep(.el-form-item__label) {
+      font-weight: 600;
+      color: #303133;
+    }
+  }
+  .uploadDN {
+    display: flex;
+  }
+}
 .upload-demo {
   display: flex;
   :deep(.el-upload-list) {
